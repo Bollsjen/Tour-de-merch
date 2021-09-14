f06d6bb6188dc1a3e9925ed45e054407022c6766
Magnus Boll Jensen <magn733m@edu.easj.dk>
2021-09-14 13:03:04 +0200

    Edit på koncert og turne

---
+++ Tour de merch/Pages/Koncerter/EditKoncert.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tour_de_merch.Interface;
+using Tour_de_merch.Module;
+
+namespace Tour_de_merch.Pages.Koncerter
+{
+    public class EditKoncertModel : PageModel
+    {
+        IKoncertRepository repo;
+        ITurneRepository turRepo;
+        [BindProperty] public Koncert Koncert { get; set; }
+        public Dictionary<int, Turne> Turneer { get; set; }
+        public EditKoncertModel(IKoncertRepository repo, ITurneRepository turRepo)
+        {
+            this.repo = repo;
+            this.turRepo = turRepo;
+        }
+
+        public void OnGet(int id)
+        {
+            Turneer = turRepo.GetAllTurneer();
+            Koncert = repo.GetKoncert(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            repo.UpdateKoncert(Koncert);
+            return RedirectToPage("Index");
+        }
+    }
+}
