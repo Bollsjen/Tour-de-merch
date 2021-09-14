d8195692095b6724d5268acdf97cb3c57646e827
AyaanEJ <princesumeel@hotmail.com>
2021-09-14 11:00:17 +0100

    added koncertitems

--- Tour de merch/Pages/Koncerter/KoncertItemKatalog/Index.cshtml.cs
+++ Tour de merch/Pages/Koncerter/KoncertItemKatalog/Index.cshtml.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tour_de_merch.Module;
+using Tour_de_merch.Interface;
+
 
 namespace Tour_de_merch.Pages.Koncerter.KoncertItemKatalog
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public Koncert koncert { get; set; }
+
+        IKoncertRepository repository;
+
+        public IndexModel(IKoncertRepository repository)
         {
+            this.repository = repository;
+        }
+        
+        
+
+        public void OnGet(int id)
+        {
+            koncert = repository.GetKoncert(id);
+            int 0;
         }
     }
 }
