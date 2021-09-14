783d9da809046ef7b198e65d08b017df211e2e38
Mai Lionett Nielsen <maix2374@edu.easj.dk>
2021-09-14 10:23:44 +0200

    Create koncert

--- Tour de merch/Pages/Koncerter/CreateKoncert.cshtml.cs
+++ Tour de merch/Pages/Koncerter/CreateKoncert.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tour_de_merch.Module;
 
 namespace Tour_de_merch.Pages.Koncerter
 {
     public class CreateKoncertModel : PageModel
     {
+
+        [BindProperty] public Koncert Koncert { get; set; }
         public void OnGet()
         {
         }
