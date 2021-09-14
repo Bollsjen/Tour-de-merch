62097ea7a4ea638edc93dd9701c277f1f81986fd
atlantis1234 <serc0024@edu.easj.dk>
2021-09-14 11:09:09 +0200

    Create Koncert

--- Tour de merch/Pages/Koncerter/CreateKoncert.cshtml.cs
+++ Tour de merch/Pages/Koncerter/CreateKoncert.cshtml.cs
@@ -5,15 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Tour_de_merch.Module;
+using Tour_de_merch.Interface;
 
 namespace Tour_de_merch.Pages.Koncerter
 {
     public class CreateKoncertModel : PageModel
     {
 
-        [BindProperty] public Koncert Koncert { get; set; }
+        IKoncertRepository repo;
+
+        [BindProperty] public Koncert koncert { get; set; }
+
+
+        public CreateKoncertModel(IKoncertRepository repo)
+        {
+            this.repo = repo;
+        }
+
+
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            repo.CreateKoncert(koncert);
+            return RedirectToPage("Index");
         }
     }
 }
